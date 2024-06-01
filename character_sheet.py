8e067b61c57fb87c5382cf87d49fea98aa993915
ARigitano <antoine@rigitano.com>
2024-06-02 00:05:10 +0200

    Associating player action with characteristic

--- character_sheet.py
+++ character_sheet.py
@@ -1,3 +1,10 @@
+strength = [16, 3]
+constitution = [14, 2]
+dexterity = [12, 1]
+intelligence = [12, 1]
+wisdom = [8, -1]
+charisma = [10, 0]
+
 inventory = []
 
 
--- monster_generator.py
+++ monster_generator.py
@@ -17,6 +17,9 @@ class Monster:
     def generate_player_actions(self, monster):
         prompt = ("The player is in front of this monster: {monster}."
                     "List three actions he can choose between to deal with the monster."
-                    "Each time the description contains an action for the player, add the action as a string in the python list actions "
+                    "Each time the description contains an action for the player, add the action as a string in the python list actions."
+                    "At the end of each action, write in parenthesis if the action which characteristic from the player will be used for that action,"
+                    "between strength, constitution, dexterity,  intelligence, wisdom or charisma."
                     "Finish the description with the list: actions = ['action1', ..., 'actionn']")
         return co.call_openai_api(prompt, max_tokens=100)
+
